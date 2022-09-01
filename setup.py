4341382632e4fe824bee25487306d785f2157946
agitama1 <93070268+agitama1@users.noreply.github.com>
2022-09-01 10:51:44 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-    name = "agi1.py",
+    name = "agisatu.py",
     version = "0.1",
-    py_modules = ['agi1'],
-    url = "https://github.com/demotomohiro/agi1",
+    py_modules = ['agisatu'],
+    url = "https://github.com/agitama1/agitama1",
     author = "demotomohiro",
     install_requires = ["pyngrok"]
 )

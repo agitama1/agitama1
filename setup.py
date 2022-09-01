b6283a40594811bf60974a53a118495df0ae07db
agitama1 <93070268+agitama1@users.noreply.github.com>
2022-09-01 10:41:38 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "agi1.py",
+    version = "0.1",
+    py_modules = ['agi1'],
+    url = "https://github.com/demotomohiro/agi1",
+    author = "demotomohiro",
+    install_requires = ["pyngrok"]
+)
